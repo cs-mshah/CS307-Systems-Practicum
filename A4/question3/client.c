33e4fab595dcd1f7dd6a2fca3211a656dbb9044b
harnaman-hk <harnaman.hk@gmail.com>
2022-05-22 16:33:28 +0530

    server client socket connection

--- A4/question3/client.c
+++ A4/question3/client.c
@@ -1,123 +1,77 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
 
-#define MAXIN 20
-#define MAXOUT 25600
-#define POLYNOMIAL 10101
-
-void decodeCRC(char *msg, int N, char *result)
+int connect_socket(int port, struct sockaddr_in *address)
 {
-	char polynomial[5];
-	char zeropoly[5] = "00000";
-	int reshead = 0;
-	int errorflag[N];
-	sprintf(polynomial, "%d", POLYNOMIAL);
-	for (int i = 0; i < 12 * N; i += 12)
-	{
-		char msgblock[12] = {0};
-		strncpy(msgblock, msg + i, 12);
-		for (int j = 0; j < 7; j++)
-		{
-			int flag;
-			if (msgblock[j] == '1')
-				flag = 1;
-			else
-				flag = 0;
-			msgblock[j] = '0';
-			for (int k = j + 1; k < j + 5; k++)
-			{
-				if (flag == 1)
-				{
-					if (msgblock[k] == polynomial[k - j])
-						msgblock[k] = '0';
-					else
-						msgblock[k] = '1';
-				}
-				else
-				{
-					if (msgblock[k] == zeropoly[k - j])
-						msgblock[k] = '0';
-					else
-						msgblock[k] = '1';
-				}
-			}
-		}
-		if (strcmp(msgblock + 8, "0000") == 0)
-			errorflag[i / 12] = 0;
-		else
-			errorflag[i / 12] = 1;
-		strncpy(result + reshead, msg + i, 8);
-		reshead += 8;
-	}
-	for (int i = 0; i < N; i++)
-	{
-		if (errorflag[i] == 1)
-		{
-			printf("Error in block %d", i);
-		}
-	}
-	fflush(stdout);
+    int server_fd = 0;
+    struct sockaddr_in serv_addr;
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+ 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+        <= 0) {
+        printf("\nInvalid address\n");
+        return -1;
+    }
+ 
+    *address = serv_addr;
+    return server_fd;
 }
 
-char *getreq(char *inbuf, int len)
+int connect_client(int server_fd, struct sockaddr_in *server_addr)
 {
-	printf("Enter N in (8*N)");
-	memset(inbuf, 0, len);
-	return fgets(inbuf, len, stdin);
+    int client_fd;
+
+    if ((client_fd = connect(server_fd, (struct sockaddr *)server_addr, sizeof(*server_addr))) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    return client_fd;
 }
 
-void client(int sockfd)
+void crc(int server_fd, int client_fd)
 {
-	int n;
-	int sndbuf;
-	char rcvbuf[MAXOUT];
-	char resbuf[MAXOUT] = {0};
-	printf("Enter N in (8*N): ");
-	scanf("%d", &sndbuf);
-	while (sndbuf != 0)
-	{
-		write(sockfd, &sndbuf, sizeof(int));
+    int divisor_n, read_length;
+    char divisor[9] = {0};
 
-		memset(rcvbuf, 0, MAXOUT);
-		n = read(sockfd, rcvbuf, MAXOUT - 1);
+    // receive n from server
+    read_length = read(server_fd, divisor, 6);
+    divisor_n = atoi(divisor);
 
-		decodeCRC(rcvbuf, sndbuf, resbuf);
-		printf("Recieved message: \n");
-		write(STDOUT_FILENO, resbuf, n);
-		printf("\nEnter N in (8*N): ");
-		scanf("%d", &sndbuf);
-	}
+    printf("n received: %d\n", divisor_n);
+    
 }
 
-struct hostent *buildServerAddr(struct sockaddr_in *serv_addr, char *serverIP, int portno)
+int main(int argc, char const *argv[])
 {
-	memset((char *)serv_addr, 0, sizeof(struct sockaddr_in));
-	serv_addr->sin_family = AF_INET;
-	inet_aton(serverIP, &(serv_addr->sin_addr));
-	serv_addr->sin_port = htons(portno);
-}
+    int server_fd = 0, valread, client_fd;
+    struct sockaddr_in server_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
 
-int main()
-{
-	char *serverIP = "0.0.0.0";
-	int sockfd, portno = 5000;
-	struct sockaddr_in serv_addr;
+    server_fd = connect_socket(PORT, &server_addr);
+    connect_client(server_fd, &server_addr);
 
-	buildServerAddr(&serv_addr, serverIP, portno);
+    send(server_fd, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    valread = read(server_fd, buffer, 1024);
+    printf("%s\n", buffer);
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    crc(server_fd, client_fd);
 
-	connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	printf("Connected to %s:%d\n", serverIP, portno);
-
-	client(sockfd);
-
-	close(sockfd);
+    // close socket
+    close(client_fd);
+    return 0;
 }
--- A4/question3/server.c
+++ A4/question3/server.c
@@ -1,57 +1,72 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <time.h>
-#define MAXREQ 25600
-#define MAXQUEUE 5
-#define POLYNOMIAL 10101
+#include <sys/socket.h>
+#include <unistd.h>
 
-void encodeCRC(char *msg, int N, char *result)
+#define PORT 8080
+
+int connect_socket(int port, struct sockaddr_in* address)
 {
-	char polynomial[5];
-	char zeropoly[5] = "00000";
-	int reshead = 0;
-	sprintf(polynomial, "%d", POLYNOMIAL);
-	for (int i = 0; i < 8 * N; i += 8)
-	{
-		char msgblock[12] = "000000000000";
-		strncpy(msgblock, msg + i, 8);
-		for (int j = 0; j < 7; j++)
-		{
-			int flag;
-			if (msgblock[j] == '1')
-				flag = 1;
-			else
-				flag = 0;
-			msgblock[j] = '0';
-			for (int k = j + 1; k < j + 5; k++)
-			{
-				if (flag == 1)
-				{
-					if (msgblock[k] == polynomial[k - j])
-						msgblock[k] = '0';
-					else
-						msgblock[k] = '1';
-				}
-				else
-				{
-					if (msgblock[k] == zeropoly[k - j])
-						msgblock[k] = '0';
-					else
-						msgblock[k] = '1';
-				}
-			}
-		}
-		strncpy(result + reshead, msg + i, 8);
-		strncpy(result + reshead + 8, msgblock + 8, 4);
-		reshead += 12;
-	}
+    int server_fd, new_socket, valread;
+    // struct sockaddr_in address;
+    int opt = 1;
+
+    // create socket fd
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // attach socket to port
+    if (setsockopt(server_fd, SOL_SOCKET,
+                   SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    (*address).sin_family = AF_INET;
+    (*address).sin_addr.s_addr = INADDR_ANY;
+    (*address).sin_port = htons(port);
+
+    // bind socket
+    if (bind(server_fd, (struct sockaddr *) address, sizeof(*address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // listen
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+   return server_fd;
 }
 
-void genMsg(char *msg, int N)
+int accept_connection(int server_fd, struct sockaddr* address)
+{
+    int new_socket, addrlen = sizeof((*address));;
+
+    // accept connection
+    printf("waiting for connection request\n");
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    return new_socket;
+}
+
+void generate_msg(char *msg, int N)
 {
 	srand(time(0));
 	for (int i = 0; i < 8 * N; i++)
@@ -61,53 +76,38 @@ void genMsg(char *msg, int N)
 	printf("Generated: %s\n", msg);
 }
 
-void server(int consockfd)
+void crc_client(int server_fd, int client_fd)
 {
-	int reqbuf;
-	int n;
-	while (1)
-	{
-		printf("Waiting for N..\n");
-		n = read(consockfd, &reqbuf, sizeof(int));
-		printf("Recvd N: %d\n", reqbuf);
-		char sndbuf[MAXREQ] = {0};
-		char resbuf[MAXREQ] = {0};
-		genMsg(sndbuf, reqbuf);
-		encodeCRC(sndbuf, reqbuf, resbuf);
-		if (n <= 0)
-			return;
-		sleep(5);
-		printf("Sending: %s\n", resbuf);
-		fflush(stdout);
-		n = write(consockfd, resbuf, strlen(resbuf));
-	}
+    int divisor;
+    char divisor_string[9];
+
+    printf("\nEnter CRC divisor(n): ");
+    scanf("%d", &divisor);
+    sprintf(divisor_string, "%d", divisor);
+
+    // send n to client
+    send(client_fd, divisor_string, strlen(divisor_string), 0);
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
-	int lstnsockfd, consockfd, clilen, portno = 5000;
-	struct sockaddr_in serv_addr, cli_addr;
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
 
-	memset((char *)&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
+    server_fd = connect_socket(PORT, &address);
+    new_socket = accept_connection(server_fd, &address);
 
-	lstnsockfd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("client connected\n");
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
 
-	bind(lstnsockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	printf("Bounded to port\n");
-	while (1)
-	{
-		printf("Listening for incoming connections\n");
-		listen(lstnsockfd, MAXQUEUE);
+    crc_client(server_fd, new_socket);
 
-		consockfd = accept(lstnsockfd, (struct sockaddr *)&cli_addr, &clilen);
-		printf("Accepted connection\n");
-
-		server(consockfd);
-
-		close(consockfd);
-	}
-	close(lstnsockfd);
+    close(new_socket);
+    shutdown(server_fd, SHUT_RDWR);
+    return 0;
 }
